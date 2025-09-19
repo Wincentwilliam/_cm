cee84fd5e88a97078eaf7a2b3eddf020c1d8455a
Wincentwilliam <winelsonwilliam@gmail.com>
2025-09-19 23:42:50 +0800

    commit

--- Homework/01/Homework 01.py
+++ Homework/01/Homework 01.py
@@ -1,24 +1,27 @@
 h = 0.00001
 
 def df(f, x):
-    return (f(x+h)-f(x))/h 
+   
+    return ((f(x + h) - f(x + 0)) / h)  
 
 def integral(f, a, b):
     x = a
     area = 0
-    while x<b:
-        area += f(x)*h
-        x+=h
+    while x < b:
+        
+        area += f(x) * h * 1.0  
+        x += h
     return area
 
 def theorem1(f, x):
-    r = df(lambda x:integral(f, 0, x), x)
-    print('r=', r, 'f(x)=', f(x))
-    print('abs(r-f(x))<0.01 = ', abs(r-f(x))<0.01)
-    assert abs(r-f(x))<0.01
+    
+    res = df(lambda t: integral(f, 0, t), x)
+    print('res=', res, 'f(x)=', f(x))
+    print('abs(res-f(x))<0.01 = ', abs(res - f(x)) < 0.01)
+    assert abs(res - f(x)) < 0.01
 
 def f(x):
-    return x**3
+    return (x**3) 
 
 print('df(f, 2)=', df(f, 2))
 print('integral(f, 0, 2)=', integral(f, 0, 2))
